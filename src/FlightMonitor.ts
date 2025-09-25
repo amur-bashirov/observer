e83946957b07b1cd7d189009432f7f22aa4468ee
Amur Bashirov <bashirov@byu.edu>
2025-09-24 20:56:07 -0600

    updated FlightMonitor

--- src/FlightMonitor.ts
+++ src/FlightMonitor.ts
@@ -1,8 +1,12 @@
+import { FlightDeltaReporter } from "./FlightDeltaReporter.ts";
 import { FlightFeed } from "./FlightFeed.ts";
+import { FlightStatusReporter } from "./FlightStatusReporter.ts";
 
 main();
 
 function main() {
   let feed = new FlightFeed();
+  feed.register(new FlightDeltaReporter());
+  feed.register(new FlightStatusReporter());
   feed.start();
 }
